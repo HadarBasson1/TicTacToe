0cbe051c6344aca0f3e59098e26ab166305c5357
Noam Lushi <noamlushi@gmail.com>
2022-12-06 18:53:28 +0200

    first commit

---
+++ app/src/main/java/com/example/tictactoe/PlayersPage.java
@@ -0,0 +1,33 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class PlayersPage extends AppCompatActivity {
+    private EditText player1;
+    private EditText player2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_players_page);
+
+        player1=findViewById(R.id.players_page_name1);
+        player2=findViewById(R.id.players_page_name2);
+
+    }
+
+    public void SubmitBtnClick (View view){
+        String player1Name = player1.getText().toString();
+        String player2Name = player2.getText().toString();
+
+        Intent intent = new Intent(this,GameDisplay.class);
+        intent.putExtra("PLAYER_NAMES",new String[] {player1Name,player2Name});
+        startActivity(intent);
+
+    }
+}
